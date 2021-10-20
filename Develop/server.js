05e6d8714e150e61f66d4c1eec7f4bc2254be2d9
Zuke27 <wstaylor27@gmail.com>
2021-10-20 13:43:50 -0400

    Updated routes folder

--- Develop/server.js
+++ Develop/server.js
@@ -1,24 +1,18 @@
 const express = require('express');
-const path = require('path');
-const noteData = require('./db/db.json');
-const PORT = 3001;
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
-app.get('/api/notes', (req, res) => res.json(noteData));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
